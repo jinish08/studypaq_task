87b36ea18f0257aa73c39c1d02c9de90e539aad2
jinish08 <jinishshah08@gmailcom>
2022-05-29 12:47:51 +0530

    implimented static authentication

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,26 @@ import { Button, Images } from "../components/main_page";
 export default function Home() {
   const [urls, setUrls] = useState([]);
   const [loading, setLoading] = useState(false);
+  const username = "test";
+  const password = "test";
 
   // makes get request to localhost:5000/images to fetch all images
   const fetchImages = async () => {
     setLoading(true);
     try {
-      const res = await fetch("/images");
+      const res = await fetch("/images", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
       const json = await res.json();
       // console.log(json);
+      if (json["message"] == "Invalid credentials") {
+        alert("Invalid username of password");
+      }
       if (json["data"]) {
         // creates shallow copy of urls array
         const data = Array.from(json["data"]);
